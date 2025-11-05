41aa079c7797033e0e8b4f832b0322b0387f11a9
Dennis Zhang <ndenniszhang@gmail.com>
2025-11-05 15:11:20 -0500

    switch to app.yaml

--- src/main/java/dev/denniszhang/gen_ai_orchestrator/GenAiOrchestratorApplication.java
+++ src/main/java/dev/denniszhang/gen_ai_orchestrator/GenAiOrchestratorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenAiOrchestratorApplication {
-
 	static void main(String[] args) {
 		SpringApplication.run(GenAiOrchestratorApplication.class, args);
 	}
-
 }
