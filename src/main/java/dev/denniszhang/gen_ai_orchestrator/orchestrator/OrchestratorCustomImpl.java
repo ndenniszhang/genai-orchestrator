56d209fef63c2b031fc21de3afde326e61beb237
Dennis Zhang <ndenniszhang@gmail.com>
2025-11-20 23:29:40 -0500

    add Docker MCP Gateway

--- src/main/java/dev/denniszhang/gen_ai_orchestrator/orchestrator/OrchestratorCustomImpl.java
+++ src/main/java/dev/denniszhang/gen_ai_orchestrator/orchestrator/OrchestratorCustomImpl.java
@@ -1,6 +1,7 @@
 package dev.denniszhang.gen_ai_orchestrator.orchestrator;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +11,10 @@ public class OrchestratorCustomImpl implements Orchestrator{
 
     private final ChatClient chatClient;
 
-    public OrchestratorCustomImpl(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public OrchestratorCustomImpl(ChatClient.Builder builder, ToolCallbackProvider tools) {
+        this.chatClient = builder
+                .defaultToolCallbacks(tools.getToolCallbacks())
+                .build();
     }
 
     public String goal(String message) {
